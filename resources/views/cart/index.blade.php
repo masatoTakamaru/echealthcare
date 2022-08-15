badfae8a758bb9ad4a3ab2bb94044b163925c232
masato <tkmasa1024@gmail.com>
2022-08-15 11:53:28 +0900

    カート設置

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -1,8 +1,8 @@
-<x-app-layout>
-<section>
+<x-guest-layout>
+<section class="m-4">
     <h1>ショッピングカート</h1>
 </section>
-<section>
+<section class="m-4">
     @if($items->count() == 0)
         <p>商品はありません。</p>
     @else
@@ -10,17 +10,17 @@
         <tbody>
         @foreach($items as $item)
             <tr>
-            <td><img class="cartItemThumbnail" src="{{ $item->associatedModel->productphotos->first()->url }}"></td>
-            <td class="cartItemColumn">
+            <td class="w-1/4 pr-4"><img src="{{ $item->associatedModel->productphotos->first()->url }}"></td>
+            <td>
                 <ul>
                 <li>{{ $item->name }}</li>
-                <li class="cartItemPrice">{{ number_format($item->price) }}&nbsp;円</li>
+                <li class="text-price">{{ number_format($item->price) }}&nbsp;円</li>
                 <li>
                     <form action="{{ route('cart.destroy', ['cart' => $item->id]) }}" method="POST">
                         @csrf
                         @method('DELETE')
-                        <div class="cartItemDeleteButtonWrapper">
-                            <input type="submit" class="cartItemDeleteButton" value="削除">
+                        <div class="mb-4">
+                            <input class="text-sm border-2 py-1 px-4 rounded-lg" type="submit" value="削除">
                         </div>
                     </form>
                 </li>
@@ -33,15 +33,15 @@
         </tr>
         <tr>
         <td></td>
-        <td class="cartSubTotal">
-            <span>小計&nbsp;:&nbsp;</span><span class="cartSubTotalPrice">{{ number_format(Cart::getSubTotal()) }}&nbsp;円</span>
+        <td>
+            <span>小計&nbsp;:&nbsp;</span><span class="text-price">{{ number_format(Cart::getSubTotal()) }}&nbsp;円</span>
         </td>
         </tr>
         </tbody>
         </table>
-        <div class="proceedToRetailCheckout">
-            <a class="proceedToRetailCheckoutButton" href="{{ route('checkout') }}">レジに進む</a>
+        <div class="my-4 text-center">
+            <a class="bg-primary py-2 px-10 rounded shadow" href="{{ route('checkout') }}">レジに進む</a>
         </div>
     @endif
 </section>
-</x-app-layout>
+</x-guest-layout>
