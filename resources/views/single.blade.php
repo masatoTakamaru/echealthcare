558d90d68a4a924ee852bf8520d3875a0fe0b871
masato <tkmasa1024@gmail.com>
2022-08-15 10:48:38 +0900

    商品ページ追加

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,41 +1,39 @@
 <x-guest-layout>
-<section>
-    <div class="singleHeader">
+<section class="p-4">
+    <div class="text-brown font-bold ml-4">
         {{ $product->header }}
     </div>
-    <img class="singleThumbnail"
-        src="{{ asset($main_photo->url) }}">
-    <div class="singlePhotos">
+    <img class="w-full p-4" src="{{ asset($main_photo->url) }}">
+    <div class="flex flex-wrap items-center">
         @foreach($product->productphotos as $photo)
-            <a href="{{ route('single', ['id' => $product->id, 'another' => $photo->id]) }}">
-                <img class="anotherThumbnail"
-                    src="{{ asset($photo->url) }}">
+            <a class="m-1 border" href="{{ route('single', ['id' => $product->id, 'another' => $photo->id]) }}">
+                <img class="w-14 p-1" src="{{ asset($photo->url) }}">
             </a>
         @endforeach
     </div>
-    <div class="singleName">
+    <div class="font-bold text-justify">
         {{ $product->name }}
     </div>
-    <div class="singleMaker">
+    <div class="py-2">
         {{ $product->maker }}
     </div>
-    <div class="singlePrice">
+    <div class="text-price text-lg mb-4">
         {{ number_format($product->price) }}円
     </div>
-    <div class="orderForm">
+    <div class="mb-4">
         @if($max_quantity)
             <form action="{{ route('cart.store') }}" method="POST">
                 @csrf
 
                 <input type="hidden" name="id" value="{{ $product->id }}">
-                <label for="quantity">数量:</label>
-                <select id="quantity" name="quantity">
+                <label for="quantity">数量&nbsp;:&nbsp;</label>
+                <select class="border-2 p-2" id="quantity" name="quantity">
                     @for($i = 1; $i <= $max_quantity; $i++)
                         <option value="{{ $i }}">{{ $i }}</option>
                     @endfor
                 </select>
-                <span>
-                    <input type="submit" class="buttonInput" value="カートに入れる" />
+                <span class="ml-4 py-2 px-4 bg-primary rounded-lg">
+                    <input type="submit" value="カートに入れる" />
                 </span>
             </form>
         @else
@@ -43,9 +41,9 @@
         @endif
     </div>
     <div class="singleSerial">
-        シリアル番号:[{{ $product->serial }}]
+        シリアル番号&nbsp;:&nbsp;[{{ $product->serial }}]
     </div>
-    <div class="singleSpec">
+    <div class="singleSpec text-justify">
         {{ $product->spec }}
     </div>
 </section>
