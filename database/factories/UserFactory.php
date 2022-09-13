8af39409430acbc5fe49f36879bd88dbf9deca94
masato <tkmasa1024@gmail.com>
2022-09-13 12:41:29 +0900

    editTest

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,11 +18,17 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->safeEmail(),
+            'email' => fake()->freeEmailDomain(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'last_name' => fake()->lastName(),
+            'first_name' => fake()->firstName(),
+            'last_name_kana' => fake()->text(20),
+            'first_name_kana' => fake()->text(20),
+            'phone' => fake()->numberBetween(1000000000,99999999999),
+            'zip' => fake()->numberBetween(1000000,9999999),
+            'address' => fake()->address(),
         ];
     }
 
