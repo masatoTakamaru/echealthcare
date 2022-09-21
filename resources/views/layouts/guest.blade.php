0a9b300240db67e7bb8c1d813136858d2f0da6f1
masato <tkmasa1024@gmail.com>
2022-09-21 11:48:56 +0900

    svg

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -24,7 +24,9 @@
     <nav class="header-nav-menu">
         <div>
             <a href="{{ route('user.search') }}">
-                <embed type="image/svg+xml" src="{{ asset('icons/ui/search.svg') }}">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                </svg>
                 <p>見つける</p>
             </a>
         </div>
