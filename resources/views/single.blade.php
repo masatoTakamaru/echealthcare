809210d96e0cc4f657b4eaf32cdb1a88faa0093e
masato <tkmasa1024@gmail.com>
2022-09-22 11:06:19 +0900

    画像アドレス変更

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -78,7 +78,7 @@
             <p>お支払い方法</p>
             <div class="payment-icons">
                 <img src="{{ asset('/images/settlements/visa.svg') }}">
-                <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="40px" height="40px"><path fill="#ff9800" d="M32 10A14 14 0 1 0 32 38A14 14 0 1 0 32 10Z"/><path fill="#d50000" d="M16 10A14 14 0 1 0 16 38A14 14 0 1 0 16 10Z"/><path fill="#ff3d00" d="M18,24c0,4.755,2.376,8.95,6,11.48c3.624-2.53,6-6.725,6-11.48s-2.376-8.95-6-11.48 C20.376,15.05,18,19.245,18,24z"/></svg>
+                <img src="{{ asset('/images/settlements/master.svg') }}">
                 <img src="{{ asset('/images/settlements/jcb.webp') }}">
                 <img src="{{ asset('/images/settlements/paypay.webp') }}">
                 <img src="{{ asset('/images/settlements/rakutenpay.webp') }}">
