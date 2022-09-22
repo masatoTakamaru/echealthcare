de70a4fd834e2056bc3d6ecc74a7adbf712be750
masato <tkmasa1024@gmail.com>
2022-09-22 12:40:22 +0900

    画像アドレス変更

--- resources/views/components/subcat-index.blade.php
+++ resources/views/components/subcat-index.blade.php
@@ -3,9 +3,7 @@
         <li>
             <a href="{{ route('user.category', ['cat_id' => $cat->id, 'subcat_id' => $subcat->id]) }}">
                 <p>{{ $subcat->name }}</p>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#888" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                </svg>
+                <img src="{{ asset('/images/ui/chevron-right-gray.svg') }}">
                 </a>
         </li>
     @endforeach
